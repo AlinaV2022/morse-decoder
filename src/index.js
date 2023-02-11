b652acb60d0c92f5abdd2c213e24738a910dfadf
Alina <vosareva1@gmail.com>
2023-02-11 20:16:33 +0300

    feat: create morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for ( let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if(letter[0] === '*') {
+            result += ' ';
+            continue;
+        }
+        let temp = '';
+        for (let j = 0; j < letter.length; j += 2) {
+            let code = letter[j] + letter[j + 1];
+            if (code === '10') {
+                temp += '.';
+            }
+            if(code === '11') {
+                temp += '-';
+            }
+        }
+        result += MORSE_TABLE[temp];
+    }
+    return result;
 }
+
 
 module.exports = {
     decode
